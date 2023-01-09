cc99286457d4f45db3b86191e85e2e764d696a61
hazhar ghaderi <hazhar24ghaderi@gmail.com>
2023-01-09 23:27:47 +0330

    update data file

---
+++ data/header.ts
@@ -0,0 +1,11 @@
+export const header = {
+  links: [
+    { name: "بیشتر", href: "/more" },
+    { name: "تماس با ما", href: "/contactUs" },
+    { name: "توسعه دهندگان", href: "/developers" },
+    { name: "تعرفه", href: "/tariff" },
+    { name: "محصولات", href: "/products" },
+  ],
+
+  button: [{ name: "زرین پال من "}],
+};
