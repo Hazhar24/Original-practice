0189711aa18470ce964c0295e3508dcca4fec6cf
hazhar ghaderi <hazhar24ghaderi@gmail.com>
2023-01-11 23:08:40 +0330

    update data file

--- data/header.ts
+++ data/header.ts
@@ -1,11 +1,61 @@
 export const header = {
   links: [
-    { name: "بیشتر", href: "/more" },
+    {
+      name: "بیشتر",
+      href: "/more",
+      submenu: [
+        {
+          href: "/weblog",
+          name: "وبلاک",
+        },
+        {
+          href: "/numberShaba",
+          name: "دریافت شماره شبا",
+        },
+        {
+          href: "/questions",
+          name: "سوالات متداول",
+        },
+        {
+          hreh: "/application",
+          name: "اپلیکیشن",
+        },
+        {
+          href: "/zarinbin",
+          name: "زرین بین",
+        },
+      ],
+    },
     { name: "تماس با ما", href: "/contactUs" },
     { name: "توسعه دهندگان", href: "/developers" },
     { name: "تعرفه", href: "/tariff" },
-    { name: "محصولات", href: "/products" },
+    {
+      name: "محصولات",
+      href: "/products",
+      submenu: [
+        {
+          href: "/weblog",
+          name: "وبلاک",
+        },
+        {
+          href: "/numberShaba",
+          name: "دریافت شماره شبا",
+        },
+        {
+          href: "/questions",
+          name: "سوالات متداول",
+        },
+        {
+          hreh: "/application",
+          name: "اپلیکیشن",
+        },
+        {
+          href: "/zarinbin",
+          name: "زرین بین",
+        },
+      ],
+    },
   ],
 
-  button: [{ name: "زرین پال من "}],
+  button: [{ name: "زرین پال من " }],
 };
