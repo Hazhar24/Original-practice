8f7362ec7d418f792fca4f9b3e94d5d722b8d8a7
hazhar ghaderi <hazhar24ghaderi@gmail.com>
2023-01-15 15:28:58 +0330

    add file footer

--- Layouts/Home.tsx
+++ Layouts/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "@/components/Home/Header";
+import Footer from "@/components/Home/Footer";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 };
