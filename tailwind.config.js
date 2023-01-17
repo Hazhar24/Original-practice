e4ff8e9ac6cb315262d84a361d9d286131cb983a
hazhar ghaderi <hazhar24ghaderi@gmail.com>
2023-01-17 15:27:00 +0330

    add config for background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url(https://www.zarinpal.com/assets/images/wave.svg?4a663924967723420f5c4cb59d15959e)",
+      },
+    },
   },
   plugins: [],
 }
