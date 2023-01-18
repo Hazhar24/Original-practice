783cf55bfbb43d88bfaaa3513f0833e2405b2067
hazhar ghaderi <hazhar24ghaderi@gmail.com>
2023-01-18 16:15:07 +0330

    create data Corect related Twosection

--- data/Content.ts
+++ data/Content.ts
@@ -1,10 +1,23 @@
 export interface TContent {
   Section1: Section1[];
+  Section2: Section2[];
   Register: Register[];
   Login: Login[];
+  Makepayment: Makepayment[];
+  moreAbout: moreAbout[];
 }
-
+export interface Makepayment {
+  name: string;
+  href?: string;
+}
+export interface moreAbout {
+  name: string;
+  href?: string;
+}
 export interface Section1 {
+  name: string;
+}
+export interface Section2 {
   name: string;
 }
 export interface Register {
@@ -17,6 +30,10 @@ export interface Login {
 }
 
 export const Content: TContent = {
+  Register: [{ name: "ثبت نام", href: "/" }],
+  Login: [{ name: "ورود", href: "/" }],
+  Makepayment: [{ name: "ساخت درگاه پرداخت", href: "/" }],
+  moreAbout: [{ name: "بیشتر بدانید", href: "/" }],
   Section1: [
     { name: "بیش از ۱۲ سال" },
     { name: "انتخاب مطمئنِ کسب و کارهای آنلاین" },
@@ -24,6 +41,12 @@ export const Content: TContent = {
       name: "زرین‌پال، اولین پرداخت‌یار پیشگام کشور، با به‌کارگیری سبک و استانداردهای جدید در ارائه‌ی خدمات درگاه پرداخت اینترنتی، سرویس‌های متنوعی در حوزه‌ی پرداخت الکترونیک را برای کسب و کارها ارائه کرده است؛ با این هدف که در افزایش سهم تجارت الکترونیکی در تولید ناخالص ملی و کمک به رشد و توسعه‌ی کسب‌وکارها، نقش سازنده‌‌ای ایفا کند",
     },
   ],
-  Register: [{ name: "ثبت نام", href: "/" }],
-  Login: [{ name: "ورود", href: "/" }],
+
+  Section2: [
+    { name: "درگاه پرداخت اینترنتی" },
+    { name: "مهندسی شده برای رشد و فروش بیش تر" },
+    {
+      name: "درگاه پرداخت زرین‌پال، با اتصالِ همزمان به درگاه‌های متنوع و معتبر بانکی(PSPها)،کاربران را به سریع‌ترین و مطمئن‌ترین درگاه بانکی منتقل می‌کند و به واسطه‌ی قابلیت مسیردهی هوشمند، باعث افزایش درصد تراکنش‌های موفق می‌شود",
+    },
+  ],
 };
